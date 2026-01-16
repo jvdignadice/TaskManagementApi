1729e72bf684191944298448be80a41363ae1322
Jay Vee Dignadice <jvdignadice@gmail.com>
2026-01-17 00:41:42 +0800

    code change cors issue

--- src/taskmanagement.api.ms/Program.cs
+++ src/taskmanagement.api.ms/Program.cs
@@ -24,7 +24,7 @@ builder.Services.AddCors(options =>
     options.AddPolicy("AllowFrontend", policy =>
     {
         policy
-            .WithOrigins("http://localhost:5173") 
+            .WithOrigins("http://localhost:5175") 
             .AllowAnyHeader()
             .AllowAnyMethod();
     });
